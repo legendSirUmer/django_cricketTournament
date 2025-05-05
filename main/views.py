74703ab4d7d020a67930152ce5e6a840bba8c45d
legendSirUmer <umerhussain17june@gmail.com>
2025-05-05 20:34:54 +0500

    'finaltouch'

--- main/views.py
+++ main/views.py
@@ -6,10 +6,10 @@ def index(request):
     matches = Match.objects.all()
     next_match = matches.filter(date__gt=datetime.date.today()).order_by('date').first()
     current_match = matches.filter(date=datetime.date.today()).first()
-    current_live_score = LiveScore.objects.filter(match=current_match).first() if next_match else None
-    print( "current liveScore id" , current_live_score.id)
-    print( "current match Team 1" , current_match.team1.name)
-    print( "current match Team 2" , current_match.team2.name)
+    current_live_score = LiveScore.objects.filter(match=current_match).first() 
+    # print( "current liveScore id" , current_live_score.id)
+    # print( "current match Team 1" , current_match.team1.name)
+    # print( "current match Team 2" , current_match.team2.name)
     
     previous_matches = matches.filter(date__lt=datetime.date.today()).order_by('-date')[:5]
     return render(request, 'index.html', {'matches': matches, 'next_match': next_match, 'previous_matches': previous_matches, 'current_match': current_live_score})
